f3c7d8257ed24dab1d354290629ecd940c51ed71
Vorabhon Chuaybhan <max003003003@gmail.com>
2018-05-24 22:25:16 +0700

    First commit

---
+++ dev/blockchain.js
@@ -0,0 +1,35 @@
+function  Blockchain () {
+    this.chain = [];
+    this.pendingTransaction = [];
+  }
+  
+Blockchain.prototype.createNewBlock= function(nouce, previousBlockHash, hash) {
+    const newBlock = {
+      index: this.chain.length + 1,
+      timestamp: Date.now(),
+      transaction: this.pendingTransaction,
+      nouce: nouce,
+      hash: hash,
+      previousBlockHash: previousBlockHash
+    };
+    this.pendingTransaction = []; //clear  transaction
+    this.chain.push(newBlock);
+    return newBlock;
+  }
+
+Blockchain.prototype.getLastBlock = funtion() {
+  return this.chain[this.chain.length -1];
+}
+
+Blockchain.prototype.createNewBlock = function(amount, sender, recipient) {
+  const newTransaction = {
+    amount: amount,
+    sender: sender,
+    recipient: recipient
+  };
+  this.pendingTransaction.push(newTransaction);
+
+  return this.getLastBlock()['index'] + 1; //return the number of the block that this transaction will be added to.
+}
+
+module.exports = Blockchain;
---
+++ dev/test.js
@@ -0,0 +1,7 @@
+const Blockchain = require('./blockchain.js');
+
+const bitcoin = new Blockchain();
+
+bitcoin.createNewBlock(2389, 'OOKDJFDKFJDF','KIIDFLSLFJDF');
+
+console.log(bitcoin);
